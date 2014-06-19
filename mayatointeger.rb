9d065ad13c58c377613b4aac1c86d82d738501f9
Simon Walther <simon.walther@liquid-concept.ch>
2014-06-19 10:49:37 +0200

    make it able to calcul the circle, the bar and the floor, and test it

--- mayatointeger.rb
+++ mayatointeger.rb
@@ -10,8 +10,44 @@ end
 class Converter
 
   def convert(number)
+    result = 0
+    bar_count = 0
+    endl_count = 0
+    floor = 0
+    floor_value = 0
+    number = number.reverse.split(//)
 
-    return integer_number
+    number.each do |char|
+      if char == "\n"
+        endl_count += 1
+
+        if endl_count == 2
+          result += (floor_value*(20**floor))
+          floor_value = 0
+          floor += 1
+          endl_count = 0
+        end
+      elsif char != "\n"
+        endl_count = 0
+
+        if char == "o"
+          floor_value += 1
+        elsif char == "-"
+          if bar_count == 0
+            bar_count += 1
+            floor_value += 5
+          elsif bar_count == 5
+            bar_count = 0
+          else
+            bar_count += 1
+          end
+        end
+      end
+    end
+
+    result += (floor_value*(20**floor))
+
+    return result
   end
 
   def verify(number)
--- spec/mayatointeger_spec.rb
+++ spec/mayatointeger_spec.rb
@@ -8,5 +8,29 @@ end
 describe MayaToInteger do
   subject { Test.new }
 
+  describe '#convert' do
+    it 'should compoud circle to 1' do
+      result = subject.maya_to_integer("ooo")
+      expect(result).to eq(3)
+    end
 
+    it 'should compoud bar to 5' do
+      result = subject.maya_to_integer("-----")
+      expect(result).to eq(5)
+    end
+
+    it 'should manage endline' do
+      result = subject.maya_to_integer("-----\n-----\n")
+      expect(result).to eq(10)
+    end
+
+    it 'should manage floors' do
+      result = subject.maya_to_integer("o\n\no\n-----\n-----\n")
+      expect(result).to eq(31)
+    end
+  end
+
+  describe '#verify' do
+
+  end
 end
